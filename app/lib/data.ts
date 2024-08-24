58b92359a221474cdedbae8fabc55d32aac5c0f7
Nix0712 <jokovicnikola06@gmail.com>
2024-08-25 01:14:46 +0200

    finished chapter 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,6 +21,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
+
     // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
